98bc133088d6b1f940f2ad48489b517d359a4683
Javier Harford <javierbsg@Javiers-MacBook-Pro.local>
2024-10-02 14:47:23 +0100

    set up proper scaffolding for testing different regions of the product

--- composables/useCheckout.js
+++ composables/useCheckout.js
@@ -1,9 +1,23 @@
 function useCheckout(productList, priceList) {
   let price = 0;
+  // if () {
+  //
+  // }
 
-  for (product of productList) {
-    price += productList[product];
+  // for (product of productList) {
+  //   price += productList[product];
+  // }
+
+  // I want to go through the price list and apply the special price
+  try {
+    let currentCost = productList.map(item => priceList[item].unitPrice);
+    console.log(currentCost);
+  } catch (error) {
+    new Error("Was not able to process your item list. Please check your item list and try again");
   }
+
+  const discountsApplied = currentCost;
+
 
   return {
     price
--- tests/checkout.spec.js
+++ tests/checkout.spec.js
@@ -3,21 +3,32 @@ import { expect, test, describe, it } from 'vitest';
 import { useCheckout } from '../composables/useCheckout';
 import { priceList } from './fixtures';
 
-describe('Checkout', () => {
+describe('Is Scaffolded correctly', () => {
   test('Tests are working', () => {
     expect(1).toBe(1)
   })
 
-  test('ICheckout function Exists', () => {
-    expect(typeof ICheckout).toMatch("function");
+  test('useCheckout function Exists', () => {
+    expect(typeof useCheckout).toMatch("function");
   })
+})
+
+describe('Has A Checkout Composable which', () => {
+  it('returns a price', () => {
+    let items = ['A', 'A'];
+    let { price } = useCheckout(items, priceList);
+    expect(price).toBe(100)
+  });
 
   it('Prices Items Correctly', () => {
-    let items = ['A', 'A', 'C', 'D', 'B'];
-    let { price } = useCheckout(items, priceList);
-  });
-
-  it('returns a price', () => {
-
   });
 })
+
+describe('Handles Errors by', () => {
+  it('Throwing an error when items cannot be parsed correctly', () => {
+      let items = ['A', 1];
+      let { price } = useCheckout(items, priceList);
+      expect(price).toThrowError();
+  });
+})
+
